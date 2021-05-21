8306b99003a2e61ca1ee58d64f0fd0de1471c5d2
duartefonseca <duartefonseca92@gmail.com>
2021-05-21 16:53:33 +0100

    Add files via upload

---
+++ storage/framework/views/a0c81a6757a7768cde9252bd56865b43050e4b08.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+<title>ASSET MANAGER</title>
+</head>
+
+<body>
+	LOGIN
+</body>
+
+</html> <?php /**PATH C:\wamp64\asset-manager\resources\views/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f53122f6bd885cb4d229a7e5f8ff3fa29409ea68.php
@@ -0,0 +1,1 @@
+WORKSPACES<?php /**PATH C:\wamp64\asset-manager\resources\views/workspaces.blade.php ENDPATH**/ ?>
